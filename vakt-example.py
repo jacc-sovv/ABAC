9c9210cae7d388c0d743ad4d073facef43e1fb9e
yluther <yanyejoy@gmail.com>
2022-10-30 20:17:13 -0600

    policies based on time and PLC status

--- vakt-example.py
+++ vakt-example.py
@@ -5,7 +5,7 @@
 # that ABAC prevents against a specifically crafted attack
 
 import vakt
-from vakt.rules import Eq, Any, StartsWith, And, Greater, Less, EndsWith, In, CIDR
+from vakt.rules import Eq, Any, StartsWith, And, Greater, GreaterOrEqual, Less, LessOrEqual, EndsWith, In, NotIn, CIDR
 
 policy = vakt.Policy(
     123456,
@@ -30,6 +30,7 @@ policy2 = vakt.Policy(
     Allow admins to get the log files
     """
 )
+
 #Example policy on a PLC resource using certain deviceIDs and example CIDR's
 policy3 = vakt.Policy(
     2,
@@ -39,7 +40,34 @@ policy3 = vakt.Policy(
     resources=[Eq('PLC')],
     effect=vakt.ALLOW_ACCESS
 )
-policies = [policy, policy2, policy3]
+
+# Example policy on a PLC during a certain time 08:00 to 17:00 i.e. 8am to 5pm as an engineer
+policy4 = vakt.Policy(
+    3,
+    actions=[Eq('get'), Eq('set')],
+    subjects=[{'name': Any(), 'role' : Eq('engineer')}],         
+    resources=[Eq('PLC')],
+    context={'time' : And(GreaterOrEqual(8.00), LessOrEqual(17.00))},
+    effect=vakt.ALLOW_ACCESS,
+    description="""
+    Allow engineers to access PLC during work hours 8am - 5pm
+    """
+)
+
+# Example policy on a PLC at a certain status as an engineer
+policy5 = vakt.Policy(
+    4,
+    actions=[Eq('get'), Eq('set')],
+    subjects=[{'name': Any(), 'role' : Eq('engineer')}],         
+    resources=[Eq('PLC')],
+    context={'status' : NotIn('Stopped')},
+    effect=vakt.ALLOW_ACCESS,
+    description="""
+    Allow engineers to access PLC when it's not stopped
+    """
+)
+
+policies = [policy, policy2, policy3, policy4, policy5]
 storage = vakt.MemoryStorage()
 for p in policies:
     storage.add(p)
@@ -57,6 +85,7 @@ assert guard.is_allowed(inq)
 inq2 = vakt.Inquiry(action='get',
                     resource='system.log',
                     subject={'name': 'jack', 'role': 'user'})
+
 print(f"Is the requets by Jack allowed? {guard.is_allowed(inq2)}")
 
 #Test action on a PLC resource with given device_id and ip
@@ -76,7 +105,31 @@ abac_inq3 = vakt.Inquiry(action='get',
                          resource='PLC',
                          subject={'name' : 'alice', 'role' : 'engineer', 'device_id' : '111.222', 
                         'ip' : '192.250.48.6'})
+
+# Correct role but wrong time
+abac_inq4 = vakt.Inquiry(action='get',
+                         resource='PLC',
+                         subject={'name' : 'yanye', 'role' : 'engineer'},
+                         context={'time' : 18.00})
+
+# Correct role PLC status is Running
+abac_inq5 = vakt.Inquiry(action='get',
+                         resource='PLC',
+                         subject={'name' : 'yanye', 'role' : 'engineer'},
+                         context={'status' : 'Running'})
+
+# Correct role PLC status is Stopped
+abac_inq6 = vakt.Inquiry(action='get',
+                         resource='PLC',
+                         subject={'name' : 'yanye', 'role' : 'engineer'},
+                         context={'status' : 'Stopped'})
+                        
                         
 print(f"Is the ABAC request by Jack allowed? (Workstation on valid network) {guard.is_allowed(abac_inq1)}")
 print(f"Is the ABAC request by eve allowed? (Wrong device ID, correct IP) {guard.is_allowed(abac_inq2)}")
 print(f"Is the ABAC request by alice allowed? (Correct deviceID, wrong IP) {guard.is_allowed(abac_inq3)}")
+
+print("===============================")
+print(f"Is the ABAC request by yanye allowed? (Correct role but wrong time) expected False --> {guard.is_allowed(abac_inq4)}")
+print(f"Is the ABAC request by yanye allowed? (Correct role, correct status) expected True --> {guard.is_allowed(abac_inq5)}")
+print(f"Is the ABAC request by yanye allowed? (Correct role but wrong status) expected False --> {guard.is_allowed(abac_inq6)}")
