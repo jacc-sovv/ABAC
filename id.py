c00a6289346df500c60bf87ba85ee423c0711d33
jaccsovv <jack.w.sovereign@gmail.com>
2023-03-03 13:32:18 -0700

    blee

---
+++ id.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+import uuid
+
+def get_id():
+    if 'nt' in os.name:
+        #print("Hey)")
+        id = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip()
+        #print((id))
+        id = id[-12:]
+        return id
+    else:
+        id = str(subprocess.check_output(['cat', '/etc/machine-id']), 'utf-8')
+        #print(id)
+        return id
+
+get_id()
